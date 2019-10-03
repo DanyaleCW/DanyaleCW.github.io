3ac38fc7999d8f3ebdb703e4d9901de9623ae612
Danyale <danyalecw@gmail.com>
2019-10-03 19:00:19 -0400

    updated project

---
+++ main.js
@@ -0,0 +1,25 @@
+//NAV BUTTONS
+let about = document.getElementById("about-nav");
+let project = document.getElementById("project-nav");
+let more = document.getElementById("more-nav");
+
+about.addEventListener("click", function() {
+  window.location.href = "#about";
+});
+project.addEventListener("click", function() {
+  window.location.href = "#project";
+});
+more.addEventListener("click", function() {
+  window.location.href = "#more"
+});
+
+//PROJECTS
+
+let women = document.getElementById("women");
+  women.addEventListener("click", function() {
+    window.location.href = "https://danyalecw.github.io/Herstorical-Women/main.html"
+  });
+
+
+
+// MORE
